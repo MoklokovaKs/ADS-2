f5f444370bc0e4830371b37b18f85828fc18f1df
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-03-09 20:34:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,51 +1,42 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-	if (n == 1) {
-		return value;
-	}
-	else {
-		return value * pown(value, n - 1);
-	}
+double result = 1.0;
+  for (uint16_t i = 1; i <= n; i++)
+    result *= value;
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-	if (n == 1) {
-		return 1;
-	}
-	else {
-		return n * fact(n - 1);
-	}
+  uint64_t result = 1;
+  for (uint16_t i = 1; i <= n; i++)
+    result *= i;
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-	return pown(x, n) / fact(n);
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-	if (count == 1) {
-		return 1;
-	}
-	else {
-		return expn(x,(count-1))+((pown(x, (count-1)) / fact(count-1)));
-	}
+  double result = 0.0;
+  for (uint16_t i = 0; i <= count; i++)
+    result += calcItem(x, i);
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-	if (count == 1) {
-		return x;
-	}
-	else {
-		return (pown( -1, (count - 1)) * (pown(x, ((2 * count) - 1)) / fact((2 * count) - 1))) + sinn(x, (count - 1));
-	}
+  double result = 0.0;
+  for (uint16_t i = 1; i <= count; i++)
+    result = result + (pown(-1, i - 1) * calcItem(x, 2 * i - 1));
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-	if (count == 1) {
-		return 1;
-	}
-	else {
-		return cosn(x, (count - 1)) + (pown(-1, (count - 1)) * (pown(x, ((2 * count) - 2)) / fact((2 * count) - 2)));
-	}
+  double result = 0.0;
+  for (uint16_t i = 1; i <= count; i++)
+    result = result + (pown(-1, i - 1) * calcItem(x, 2 * i - 2));
+  return result;
 }
